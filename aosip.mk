0b6c3fd9200cca289edb4aee2d6e7bc6b92091bd
NurKeinNeid <mralexman3000@gmail.com>
2019-07-14 17:56:56 +0200

    cheeseburger: Derp-ify
     * Update dependencies
     * Enable inline GApps Building
     * Remove unused maintainer prop
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -24,9 +24,15 @@ $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 # Inherit some common AOSiP stuff.
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-# Maintainer Prop
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="David Sn"
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/config.mk)
+
+# PixelStyle
+$(call inherit-product-if-exists, vendor/pixelstyle/config.mk)
+
+# GApps targets
+TARGET_GAPPS_ARCH := arm64
+IS_PHONE := true
 
 PRODUCT_NAME := aosip_cheeseburger
 PRODUCT_DEVICE := cheeseburger
